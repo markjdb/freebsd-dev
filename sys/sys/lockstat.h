50d56d4b51f9a82b7987fd47b9103441edc5259a
Mark Johnston <markjdb@gmail.com>
2015-07-04 19:11:54 -0700

    lockstat: use a reader/writer flag for acquire and release probes
    lockstat(1) previously used the function name of the probe to determine
    whether a read or write lock was being acquired or released. Use a flag
    argument instead, as is done for other rw and sx probes. Update
    lockstat(1) accordingly.

--- sys/sys/lockstat.h
+++ sys/sys/lockstat.h
@@ -87,9 +87,19 @@ SDT_PROBE_DECLARE(lockstat, , , thread__spin);
 	LOCKSTAT_RECORD0(probe, lp);					\
 } while (0)
 
+#define	LOCKSTAT_PROFILE_OBTAIN_RWLOCK_SUCCESS(probe, lp, c, wt, f, l, a) do { \
+	lock_profile_obtain_lock_success(&(lp)->lock_object, c, wt, f, l); \
+	LOCKSTAT_RECORD1(probe, lp, a);					\
+} while (0)
+
 #define	LOCKSTAT_PROFILE_RELEASE_LOCK(probe, lp) do {			\
 	lock_profile_release_lock(&(lp)->lock_object);			\
 	LOCKSTAT_RECORD0(probe, lp);					\
+} while (0)
+
+#define	LOCKSTAT_PROFILE_RELEASE_RWLOCK(probe, lp, a) do {		\
+	lock_profile_release_lock(&(lp)->lock_object);			\
+	LOCKSTAT_RECORD1(probe, lp, a);					\
 } while (0)
 
 #define	LOCKSTAT_WRITER		0
@@ -107,9 +117,13 @@ extern uint64_t lockstat_nsecs(void);
 
 #define	LOCKSTAT_PROFILE_OBTAIN_LOCK_SUCCESS(probe, lp, c, wt, f, l)	\
 	lock_profile_obtain_lock_success(&(lp)->lock_object, c, wt, f, l)
+#define	LOCKSTAT_PROFILE_OBTAIN_RWLOCK_SUCCESS(probe, lp, c, wt, f, l, a) \
+	LOCKSTAT_PROFILE_OBTAIN_LOCK_SUCCESS(probe, lp, c, wt, f, l)
 
 #define	LOCKSTAT_PROFILE_RELEASE_LOCK(probe, lp)  			\
 	lock_profile_release_lock(&(lp)->lock_object)
+#define	LOCKSTAT_PROFILE_RELEASE_RWLOCK1(probe, lp, a)  		\
+	LOCKSTAT_PROFILE_RELEASE_LOCK(probe, lp)
 
 #endif /* !KDTRACE_HOOKS */
 #endif /* _KERNEL */
