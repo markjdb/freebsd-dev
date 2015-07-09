714d80422a22b909fbeffc524b503ab05014b613
Mark Johnston <markjdb@gmail.com>
2015-07-09 13:59:13 -0700

    kern_dump.c add kernel support for compressed kernel dumps
    The interface for enabling dump compression mirrors that for compressed
    user program core dumps. Specifically, the kern.compress_kernel_dumps
    sysctl determines whether kernel dumps will be compressed, and
    kern.compress_kernel_dumps_gzlevel specifies the compression level.
    The compression sysctl has no effect until a dumper is chosen, since the
    compression buffer size is determined by the max I/O size of the
    underlying dump device.
    
    The implementation works by interposing a compression stream between the
    MI kernel dump routines and the raw dump device. The compression stream
    flushes to disk whenever its input buffer is full, and once in
    dump_finish(), so no modifications to (mini)dumpsys are needed.
    
    The kernel dump header is modified slightly: in addition to the
    dumplength field, which gives the size of the dump, we now also have a
    dumpextent field, which gives the space between kernel dump headers.
    With uncompressed dumps, these are equivalent. The kernel dump header
    version is bumped accordingly, and we add a new header magic type,
    GZDUMPMAGIC.

--- sys/sys/kerneldump.h
+++ sys/sys/kerneldump.h
@@ -61,10 +61,11 @@ struct kerneldumpheader {
 	char		magic[20];
 #define	KERNELDUMPMAGIC		"FreeBSD Kernel Dump"
 #define	TEXTDUMPMAGIC		"FreeBSD Text Dump"
+#define	GZDUMPMAGIC		"FreeBSD GZIP Dump"
 #define	KERNELDUMPMAGIC_CLEARED	"Cleared Kernel Dump"
 	char		architecture[12];
 	uint32_t	version;
-#define	KERNELDUMPVERSION	1
+#define	KERNELDUMPVERSION	2
 	uint32_t	architectureversion;
 #define	KERNELDUMP_AARCH64_VERSION	1
 #define	KERNELDUMP_AMD64_VERSION	2
@@ -75,10 +76,11 @@ struct kerneldumpheader {
 #define	KERNELDUMP_SPARC64_VERSION	1
 #define	KERNELDUMP_TEXT_VERSION		1
 	uint64_t	dumplength;		/* excl headers */
+	uint64_t	dumpextent;		/* space between headers */
 	uint64_t	dumptime;
 	uint32_t	blocksize;
 	char		hostname[64];
-	char		versionstring[192];
+	char		versionstring[184];
 	char		panicstring[192];
 	uint32_t	parity;
 };
@@ -105,20 +107,20 @@ struct dump_pa {
 	vm_paddr_t pa_size;
 };
 
-int dumpsys_generic(struct dumperinfo *);
+int	dumpsys_generic(struct dumperinfo *);
 
-void dumpsys_map_chunk(vm_paddr_t, size_t, void **);
+void	dumpsys_map_chunk(vm_paddr_t, size_t, void **);
 typedef int dumpsys_callback_t(struct dump_pa *, int, void *);
-int dumpsys_foreach_chunk(dumpsys_callback_t, void *);
-int dumpsys_cb_dumpdata(struct dump_pa *, int, void *);
-int dumpsys_buf_write(struct dumperinfo *, char *, size_t);
-int dumpsys_buf_flush(struct dumperinfo *);
+int	dumpsys_foreach_chunk(dumpsys_callback_t, void *);
+int	dumpsys_cb_dumpdata(struct dump_pa *, int, void *);
+int	dumpsys_buf_write(struct dumperinfo *, char *, size_t);
+int	dumpsys_buf_flush(struct dumperinfo *);
 
-void dumpsys_gen_pa_init(void);
+void	dumpsys_gen_pa_init(void);
 struct dump_pa *dumpsys_gen_pa_next(struct dump_pa *);
-void dumpsys_gen_wbinv_all(void);
-void dumpsys_gen_unmap_chunk(vm_paddr_t, size_t, void *);
-int dumpsys_gen_write_aux_headers(struct dumperinfo *);
+void	dumpsys_gen_wbinv_all(void);
+void	dumpsys_gen_unmap_chunk(vm_paddr_t, size_t, void *);
+int	dumpsys_gen_write_aux_headers(struct dumperinfo *);
 
 int	dump_start(struct dumperinfo *, struct kerneldumpheader *);
 int	dump_finish(struct dumperinfo *, struct kerneldumpheader *);
