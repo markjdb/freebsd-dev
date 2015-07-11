136d78eba304e8a0212fc386c5c688730e28518f
Mark Johnston <markjdb@gmail.com>
2015-07-11 14:42:54 -0700

    savecore: add support for gzip kernel dumps
    Most of the changes are little touchups:
    - Add a subroutine, compare_magic, for comparing the magic field of a
      dump header.
    - We now use the dumpextent field of the trailing dump header to locate
      the leading dump header.
    - DoRegularFile needs to do reads in multiples of the device's sector
      size, but with compressed dumps we may need to do a read that doesn't
      end on a sector boundary.

--- sbin/savecore/savecore.c
+++ sbin/savecore/savecore.c
@@ -298,12 +298,19 @@ check_space(const char *savedir, off_t dumpsize, int bounds)
 	return (1);
 }
 
+static int
+compare_magic(struct kerneldumpheader *kdh, const char *magic)
+{
+
+	return (strncmp(magic, kdh->magic, sizeof(kdh->magic)));
+}
+
 #define BLOCKSIZE (1<<12)
 #define BLOCKMASK (~(BLOCKSIZE-1))
 
 static int
-DoRegularFile(int fd, off_t dumpsize, char *buf, const char *device,
-    const char *filename, FILE *fp)
+DoRegularFile(int fd, off_t dumpsize, u_int sectorsize, int sparse, char *buf,
+    const char *device, const char *filename, FILE *fp)
 {
 	int he, hs, nr, nw, wl;
 	off_t dmpcnt, origsize;
@@ -315,8 +322,8 @@ DoRegularFile(int fd, off_t dumpsize, char *buf, const char *device,
 		wl = BUFFERSIZE;
 		if (wl > dumpsize)
 			wl = dumpsize;
-		nr = read(fd, buf, wl);
-		if (nr != wl) {
+		nr = read(fd, buf, roundup(wl, sectorsize));
+		if (nr != (int)roundup(wl, sectorsize)) {
 			if (nr == 0)
 				syslog(LOG_WARNING,
 				    "WARNING: EOF on dump device");
@@ -325,7 +332,7 @@ DoRegularFile(int fd, off_t dumpsize, char *buf, const char *device,
 			nerr++;
 			return (-1);
 		}
-		if (compress) {
+		if (!sparse) {
 			nw = fwrite(buf, 1, wl, fp);
 		} else {
 			for (nw = 0; nw < nr; nw = he) {
@@ -448,13 +455,13 @@ DoFile(const char *savedir, const char *device)
 	static char infoname[PATH_MAX], corename[PATH_MAX], linkname[PATH_MAX];
 	static char *buf = NULL;
 	struct kerneldumpheader kdhf, kdhl;
-	off_t mediasize, dumpsize, firsthd, lasthd;
+	off_t mediasize, dumpextent, dumplength, firsthd, lasthd;
 	FILE *info, *fp;
 	mode_t oumask;
 	int fd, fdinfo, error;
 	int bounds, status;
 	u_int sectorsize, xostyle;
-	int istextdump;
+	int istextdump, isgzipdump;
 
 	bounds = getbounds();
 	mediasize = 0;
@@ -508,8 +515,8 @@ DoFile(const char *savedir, const char *device)
 		    (long long)lasthd, device);
 		goto closefd;
 	}
-	istextdump = 0;
-	if (strncmp(kdhl.magic, TEXTDUMPMAGIC, sizeof kdhl) == 0) {
+	isgzipdump = istextdump = 0;
+	if (compare_magic(&kdhl, TEXTDUMPMAGIC) == 0) {
 		if (verbose)
 			printf("textdump magic on last dump header on %s\n",
 			    device);
@@ -523,8 +530,8 @@ DoFile(const char *savedir, const char *device)
 			if (force == 0)
 				goto closefd;
 		}
-	} else if (memcmp(kdhl.magic, KERNELDUMPMAGIC, sizeof kdhl.magic) ==
-	    0) {
+	} else if (compare_magic(&kdhl, KERNELDUMPMAGIC) == 0 ||
+	    compare_magic(&kdhl, GZDUMPMAGIC) == 0) {
 		if (dtoh32(kdhl.version) != KERNELDUMPVERSION) {
 			syslog(LOG_ERR,
 			    "unknown version (%d) in last dump header on %s",
@@ -533,6 +540,12 @@ DoFile(const char *savedir, const char *device)
 			status = STATUS_BAD;
 			if (force == 0)
 				goto closefd;
+		}
+		if (compare_magic(&kdhl, GZDUMPMAGIC) == 0) {
+			isgzipdump = 1;
+			if (compress && verbose)
+				printf("dump is already compressed\n");
+			compress = 0;
 		}
 	} else {
 		if (verbose)
@@ -543,12 +556,10 @@ DoFile(const char *savedir, const char *device)
 		if (force == 0)
 			goto closefd;
 
-		if (memcmp(kdhl.magic, KERNELDUMPMAGIC_CLEARED,
-			    sizeof kdhl.magic) == 0) {
+		if (compare_magic(&kdhl, KERNELDUMPMAGIC_CLEARED) == 0) {
 			if (verbose)
 				printf("forcing magic on %s\n", device);
-			memcpy(kdhl.magic, KERNELDUMPMAGIC,
-			    sizeof kdhl.magic);
+			memcpy(kdhl.magic, KERNELDUMPMAGIC, sizeof kdhl.magic);
 		} else {
 			syslog(LOG_ERR, "unable to force dump - bad magic");
 			goto closefd;
@@ -576,8 +587,9 @@ DoFile(const char *savedir, const char *device)
 		if (force == 0)
 			goto closefd;
 	}
-	dumpsize = dtoh64(kdhl.dumplength);
-	firsthd = lasthd - dumpsize - sizeof kdhf;
+	dumpextent = dtoh64(kdhl.dumpextent);
+	dumplength = dtoh64(kdhl.dumplength);
+	firsthd = lasthd - dumpextent - sizeof kdhf;
 	if (lseek(fd, firsthd, SEEK_SET) != firsthd ||
 	    read(fd, &kdhf, sizeof(kdhf)) != sizeof(kdhf)) {
 		syslog(LOG_ERR,
@@ -622,7 +634,7 @@ DoFile(const char *savedir, const char *device)
 	if (verbose)
 		printf("Checking for available free space\n");
 
-	if (!check_space(savedir, dumpsize, bounds)) {
+	if (!check_space(savedir, dumplength, bounds)) {
 		nerr++;
 		goto closefd;
 	}
@@ -648,6 +660,9 @@ DoFile(const char *savedir, const char *device)
 		snprintf(corename, sizeof(corename), "%s.%d.gz",
 		    istextdump ? "textdump.tar" : "vmcore", bounds);
 		fp = zopen(corename, "w");
+	} else if (isgzipdump) {
+		snprintf(corename, sizeof(corename), "vmcore.%d.gz", bounds);
+		fp = fopen(corename, "w");
 	} else {
 		snprintf(corename, sizeof(corename), "%s.%d",
 		    istextdump ? "textdump.tar" : "vmcore", bounds);
@@ -688,15 +703,15 @@ DoFile(const char *savedir, const char *device)
 	fclose(info);
 
 	syslog(LOG_NOTICE, "writing %score to %s/%s",
-	    compress ? "compressed " : "", savedir, corename);
+	    compress || isgzipdump ? "compressed " : "", savedir, corename);
 
 	if (istextdump) {
-		if (DoTextdumpFile(fd, dumpsize, lasthd, buf, device,
+		if (DoTextdumpFile(fd, dumplength, lasthd, buf, device,
 		    corename, fp) < 0)
 			goto closeall;
 	} else {
-		if (DoRegularFile(fd, dumpsize, buf, device, corename, fp)
-		    < 0)
+		if (DoRegularFile(fd, dumplength, sectorsize,
+		    !(compress || isgzipdump), buf, device, corename, fp) < 0)
 			goto closeall;
 	}
 	if (verbose)
@@ -713,7 +728,7 @@ DoFile(const char *savedir, const char *device)
 		syslog(LOG_WARNING, "unable to create symlink %s/%s: %m",
 		    savedir, "info.last");
 	}
-	if (compress) {
+	if (compress || isgzipdump) {
 		snprintf(linkname, sizeof(linkname), "%s.last.gz",
 		    istextdump ? "textdump.tar" : "vmcore");
 	} else {
