1d03363addf9d85aa05f07b088e0d96905a61033
Mark Johnston <markjdb@gmail.com>
2015-10-11 17:17:40 -0700

    Improve hash table management.

--- sys/cddl/dev/sdt/sdt.h
+++ sys/cddl/dev/sdt/sdt.h
@@ -3,13 +3,20 @@
 
 struct sdt_probedesc;
 
-struct sdt_siterec {
-	struct sdt_probedesc	*desc;
-	LIST_ENTRY(sdt_siterec) next;
-	dtrace_id_t		id;
+/*
+ * An entry in the SDT hash table. These records exist one-to-one with SDT probe
+ * descriptors, but are split into a separate struct to avoid bloat: descriptors
+ * are created at compile-time and always reside in memory. Note that multiple
+ * records may have the same id if the probe definition hard-codes a function
+ * name.
+ */
+struct sdt_invoprec {
+	struct sdt_probedesc	*sr_desc;
+	LIST_ENTRY(sdt_invoprec) sr_next;
+	dtrace_id_t		sr_id;
 };
 
-struct sdt_siterec *sdt_lookup_site(uint64_t);
+struct sdt_invoprec *sdt_lookup_site(uint64_t);
 int	sdt_invop(uintptr_t, uintptr_t *, uintptr_t);
 void	sdt_probe_enable(struct sdt_probedesc *);
 void	sdt_probe_disable(struct sdt_probedesc *);
