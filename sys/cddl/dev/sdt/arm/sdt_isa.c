ffd5107d93912ea8083e65f596811777c90fd7cf
Mark Johnston <markjdb@gmail.com>
2016-03-20 23:23:25 -0700

    Add arm stubs

---
+++ sys/cddl/dev/sdt/arm/sdt_isa.c
@@ -0,0 +1,29 @@
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/param.h>
+#include <sys/queue.h>
+#include <sys/sdt.h>
+
+#include <sys/dtrace.h>
+
+#include <machine/frame.h>
+
+#include "sdt.h"
+
+int
+sdt_invop(uintptr_t addr, uintptr_t *stack, uintptr_t rval)
+{
+
+	return (DTRACE_INVOP_NOP);
+}
+
+void
+sdt_probe_enable(struct sdt_probedesc *desc)
+{
+}
+
+void
+sdt_probe_disable(struct sdt_probedesc *desc)
+{
+}
