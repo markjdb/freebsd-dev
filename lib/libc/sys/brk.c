0b327674e835a721365c9c4d6424fbe55ada4fb1
Mark Johnston <markjdb@gmail.com>
2018-06-01 18:22:30 -0400

    Rewrite brk()/sbrk() to avoid using _end.
    _end is a special symbol emitted by the static linker.  It is supposed
    to follow the .bss section in memory and is used by libc to set the
    initial break (curbrk and minbrk).  GNU ld and lld have differing
    behaviour around this symbol, leading to incompatibilities when libc.so
    and an executable using brk()/sbrk() are linked using different linkers.
    For instance, libc.so may end up using its own internal definition of
    _end instead of that of the executable.  Some details are described in
    PR 228574.
    
    To avoid issues of compatibility, rewrite brk() and sbrk() to avoid
    using _end.  To accomplish this, we add a return value to the kernel's
    break() system call and change its error handling slightly.  Previously,
    break() would return EINVAL if the new break address was smaller than
    the address of the beginning of the data segment.  With this change,
    such an input has no effect.  The syscall returns its notion of the
    break address (always page-aligned), so break(0) can be used to query
    the kernel for the current break address.  I believe this change is
    backwards-compatible.  Further, note that brk() implementations
    silently clamp the input break address to minbrk, so brk(0) behaves the
    same now as it did before.  The syscall itself is undocumented and not
    exported by libc.
    
    Before this change, the brk() and sbrk() libc functions were implemented
    separately for each supported platform.  This change consolidates the
    implementations with a rewrite in C and adds a few simple tests.  Both
    brk() and sbrk() take care to initialize curbrk and minbrk upon first
    use, incurring a penalty of one extra system call per process which uses
    these interfaces.  Given that brk() and sbrk() are rarely used today, I
    think this is acceptable.
    
    This change also adds sbrk.o to NOASM on all architectures.  The sbrk()
    system call is and has always been a no-op.

---
+++ lib/libc/sys/brk.c
@@ -0,0 +1,105 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause-FreeBSD
+ *
+ * Copyright (c) 2018 Mark Johnston <markj@FreeBSD.org>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
+
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <errno.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void *__sys_break(char *nsize);
+
+static const void *curbrk, *minbrk;
+static int curbrk_initted;
+
+static int
+initbrk(void)
+{
+
+	if (!curbrk_initted) {
+		curbrk = minbrk = __sys_break(NULL);
+		if (curbrk == (void *)-1)
+			return (-1);
+		curbrk_initted = 1;
+	}
+	return (0);
+}
+
+static void *
+mvbrk(void *addr)
+{
+	void *newbrk;
+
+	if ((uintptr_t)addr < (uintptr_t)minbrk) {
+		/* Emulate legacy error handling in the syscall. */
+		errno = EINVAL;
+		return ((void *)-1);
+	}
+	newbrk = __sys_break(addr);
+	if (newbrk == (void *)-1)
+		return (newbrk);
+	curbrk = addr;
+	return (addr);
+}
+
+int
+brk(const void *addr)
+{
+
+	if (initbrk() == -1)
+		return (-1);
+	if ((uintptr_t)addr < (uintptr_t)minbrk)
+		addr = minbrk;
+	return (mvbrk(__DECONST(void *, addr)) == (void *)-1 ? -1 : 0);
+}
+
+int
+_brk(const void *addr)
+{
+
+	if (initbrk() == -1)
+		return (-1);
+	return (mvbrk(__DECONST(void *, addr)) == (void *)-1 ? -1 : 0);
+}
+
+void *
+sbrk(intptr_t incr)
+{
+
+	if (initbrk() == -1)
+		return ((void *)-1);
+	if ((incr > 0 && (uintptr_t)curbrk + incr < (uintptr_t)curbrk) ||
+	    (incr < 0 && (uintptr_t)curbrk + incr > (uintptr_t)curbrk)) {
+		/* Emulate legacy error handling in the syscall. */
+		errno = EINVAL;
+		return ((void *)-1);
+	}
+	return (mvbrk((void *)((uintptr_t)curbrk + incr)));
+}
