51e6940d2241af6aa67192db9878a0821a24aa3b
Mark Johnston <markjdb@gmail.com>
2019-06-14 13:22:12 -0400

    Microoptimize the libdwarf error handler.

--- contrib/elftoolchain/libdwarf/libdwarf_error.c
+++ contrib/elftoolchain/libdwarf/libdwarf_error.c
@@ -28,18 +28,24 @@
 
 ELFTC_VCSID("$Id: libdwarf_error.c 2070 2011-10-27 03:05:32Z jkoshy $");
 
+static void
+_dwarf_fill_error(Dwarf_Error *error, int errorcode, int elferrorcode,
+    const char *functionname, int linenumber)
+{
+
+	error->err_error = errorcode;
+	error->err_elferror = elferrorcode;
+	error->err_func = functionname;
+	error->err_line = linenumber;
+	error->err_msg[0] = '\0';
+}
+
 void
 _dwarf_set_error(Dwarf_Debug dbg, Dwarf_Error *error, int errorcode,
     int elferrorcode, const char *functionname, int linenumber)
 {
 	Dwarf_Error de;
 
-	de.err_error = errorcode;
-	de.err_elferror = elferrorcode;
-	de.err_func  = functionname;
-	de.err_line  = linenumber;
-	de.err_msg[0] = '\0';
-	
 	/*
 	 * If the user supplied a destination for the error, copy the
 	 * error descriptor over and return.  Otherwise, if the debug
@@ -47,9 +53,16 @@ _dwarf_set_error(Dwarf_Debug dbg, Dwarf_Error *error, int errorcode,
 	 * Otherwise, if a 'default' error handler was registered,
 	 * invoke it.
 	 */
-	if (error)
-		*error = de;
-	else if (dbg && dbg->dbg_errhand)
+
+	if (error) {
+		_dwarf_fill_error(error, errorcode, elferrorcode, functionname,
+		    linenumber);
+		return;
+	}
+
+	_dwarf_fill_error(&de, errorcode, elferrorcode, functionname,
+	    linenumber);
+	if (dbg && dbg->dbg_errhand)
 		dbg->dbg_errhand(de, dbg->dbg_errarg);
 	else if (_libdwarf.errhand)
 		_libdwarf.errhand(de, _libdwarf.errarg);
